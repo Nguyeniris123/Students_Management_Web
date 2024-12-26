a5e5b38f91c12453583d390d93a4969b260eefe9
Hồ Chí Nguyên <136091628+Nguyeniris123@users.noreply.github.com>
2024-12-26 17:47:29 +0700

    up

--- Students_Management/app/dao.py
+++ Students_Management/app/dao.py
@@ -1,4 +1,14 @@
+<<<<<<< Updated upstream
 # from app.models import Category
+=======
+from datetime import datetime
+import cloudinary
+from sqlalchemy import func
+from sqlalchemy.exc import SQLAlchemyError
+from app.models import User, Score, ScoreType, Student, Gender, Year, Semester, Subject, Regulation, RegulationAge, \
+    RegulationMaxStudent
+from app import app, db
+>>>>>>> Stashed changes
 import hashlib
 from __init__ import db
 from models import Student, User, UserRole, SexType, Class
@@ -108,6 +118,7 @@ def load_user_student(kw=None):
         users = User.query.order_by('id').filter_by(user_role=UserRole.USER)
     return users.all()
 
+<<<<<<< Updated upstream
 def update_user(lastname, firstname, phone, email, gender, dayofbirth, address):
     user = find_user_by_email_phone(phone, email)
     if gender.__eq__("male"):
@@ -121,3 +132,212 @@ def update_user(lastname, firstname, phone, email, gender, dayofbirth, address):
     user.address = address
     db.session.commit()
     load_user_student()
+=======
+def change_student_class(student_id, new_class_id):
+    try:
+        # Tìm học sinh theo ID
+        student = Student.query.get(student_id)
+
+        # Cập nhật class_id
+        student.class_id = new_class_id
+        db.session.commit()
+        return {'success': True, 'message': 'Chuyển lớp chưa xác định thành công.'}
+    except Exception as e:
+        db.session.rollback()
+        return {'success': False, 'message': 'Không thể chuyển sang lớp chưa xác định'}
+
+def add_student_to_class(student_id, class_id):
+    try:
+        # Tìm học sinh theo ID
+        student = Student.query.get(student_id)
+        if not student:
+            return {'success': False, 'message': 'Học sinh không tồn tại.'}
+
+        # Cập nhật class_id của học sinh
+        student.class_id = class_id
+        db.session.commit()
+        return {'success': True, 'message': 'Thêm học sinh vào lớp thành công.'}
+    except Exception as e:
+        db.session.rollback()
+        return {'success': False, 'message': str(e)}
+
+
+
+# Lay ra nhung hoc sinh hoc mon hoc
+def get_all_students_average_score(subject_id):
+    return db.session.query(Score.student_id,Score.subject_id,(func.sum(Score.so_diem * ScoreType.he_so)/func.sum(ScoreType.he_so)).label('DiemTrungBinh'))\
+        .filter(Score.subject_id.__eq__(subject_id)).join(ScoreType,ScoreType.id.__eq__(Score.score_type_id)).group_by(Score.student_id).all()
+
+
+def get_students_by_class(class_id, keyword=None):
+    query = Student.query.filter_by(class_id=class_id)
+    if keyword:
+        query = query.filter(Student.name.like(f"%{keyword}%"))
+    return query.order_by(Student.name).all()
+
+
+def get_scores_by_subject(subject_id):
+    return Score.query.filter_by(subject_id=subject_id).all()
+
+
+def add_score(student_id, subject_id, score_type_name, score_value):
+    try:
+        # Lấy loại điểm từ bảng ScoreType
+        score_type = ScoreType.query.filter_by(name=score_type_name).first()
+        if not score_type:
+            return {"success": False, "message": "Không tìm thấy loại điểm."}
+
+        # Tạo điểm mới
+        new_score = Score(
+            student_id=student_id,
+            subject_id=subject_id,
+            score_type_id=score_type.id,
+            so_diem=score_value
+        )
+
+        db.session.add(new_score)
+        db.session.commit()
+        return {"success": True, "message": "Thêm điểm thành công!"}
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        return {"success": False, "message": 'Lỗi: Điểm phải từ 0 - 10, Số cột điểm không được vượt quá quy định'}
+
+
+def edit_score(score_id, new_value):
+    try:
+        # Tìm điểm cần sửa
+        score = Score.query.get(score_id)
+        if not score:
+            return {"success": False, "message": "Không tìm thấy điểm."}
+
+        # Cập nhật giá trị mới
+        score.so_diem = new_value
+        db.session.commit()
+        return {"success": True, "message": "Sửa điểm thành công!"}
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        return {"success": False, "message": 'Lỗi: Điểm phải từ 0 - 10, Số cột điểm không được vượt quá quy định'}
+
+
+def delete_score(score_id):
+    try:
+        # Tìm điểm cần xóa
+        score = Score.query.get(score_id)
+        if not score:
+            return {"success": False, "message": "Không tìm thấy điểm."}
+
+        # Xóa điểm
+        db.session.delete(score)
+        db.session.commit()
+        return {"success": True, "message": "Xóa điểm thành công!"}
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        return {"success": False, "message": 'Lỗi'}
+
+
+def load_year():
+    return Year.query.order_by('id').all()
+
+
+def load_semester(id=None):
+    if id:
+        return Semester.query.order_by('id').filter_by(year_id=id).all()
+
+
+def load_subject(semester_id=None, class_grade_id=None):
+    if semester_id and class_grade_id:
+        return Subject.query.order_by('id').filter_by(semester_id=semester_id, class_grade_id=class_grade_id).all()
+
+
+def load_score_type():
+    return ScoreType.query.order_by('id').all()
+
+
+def load_score(score_type_id=None, student_id=None, subject_id=None):
+    query = Score.query
+    if score_type_id and student_id and subject_id:
+        return query.filter_by(student_id=student_id, subject_id=subject_id, score_type_id=score_type_id).all()
+
+
+def load_regulation(id=None):
+    if id:
+        return Regulation.query.get(id)
+    return Regulation.query.order_by('id').all()
+
+
+def load_detail_regulation(id=None):
+    if id:
+        if RegulationAge.query.get(id):
+            return RegulationAge.query.filter(RegulationAge.id.__eq__(id)).first()
+        if RegulationMaxStudent.query.get(id):
+            return RegulationMaxStudent.query.filter(RegulationMaxStudent.id.__eq__(id)).first()
+
+
+def change_password(student_id=None, password=None, new_password=None):
+    if student_id and password and new_password:
+        password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+        new_password = str(hashlib.md5(new_password.strip().encode('utf-8')).hexdigest())
+        user = User.query.filter(User.id.__eq__(student_id), User.password.__eq__(password)).first()
+        if user:
+            user.password = new_password
+            db.session.commit()
+            return True
+        else:
+            return False
+
+
+def change_information(student_id=None, email=None, phone=None, address=None, avatar=None):
+    if student_id:
+        user = User.query.get(student_id)
+        if user:
+            if email:
+                user.email = email
+                db.session.commit()
+            if phone:
+                user.phone = phone
+                db.session.commit()
+            if address:
+                user.address = address
+                db.session.commit()
+            if avatar:
+                res = cloudinary.uploader.upload(avatar)
+                user.avatar = res.get('secure_url')
+                db.session.commit()
+            if email or phone or address or avatar:
+                return True
+
+    return False
+
+
+def check_password(new_password=None, confirm_password=None):
+    if confirm_password and new_password:
+        if confirm_password.__eq__(new_password):
+            return True
+    return False
+
+
+def get_classgrade_id(student_id: None):
+    if student_id:
+        student = Student.query.get(student_id)
+        if student:
+            return student.class_.class_grade.id
+
+
+def get_average_score(subject_id=None, student_id=None):
+    if student_id and subject_id:
+        query = db.session.query(
+            (func.sum(Score.so_diem * ScoreType.he_so) / func.sum(ScoreType.he_so))
+        ).select_from(Score)  # select_from được gọi đầu tiên
+
+        # Thêm join sau khi select_from
+        query = query.join(ScoreType, ScoreType.id == Score.score_type_id)
+
+        # Thêm filter sau khi join
+        query = query.filter(
+            Score.subject_id == subject_id,
+            Score.student_id == student_id
+        )
+
+        # Thực hiện nhóm dữ liệu và trả về kết quả
+        return query.group_by(Score.subject_id).scalar()
+>>>>>>> Stashed changes
--- Students_Management/app/index.py
+++ Students_Management/app/index.py
@@ -1,7 +1,15 @@
 import math
 from flask import render_template, request
 import dao
+<<<<<<< Updated upstream
 from __init__ import app
+=======
+from app import app, login, db
+from flask_login import login_user, logout_user, current_user
+from app.dao import get_user_by_id
+from app.models import UserRole
+
+>>>>>>> Stashed changes
 
 @app.route("/")
 def index():
@@ -14,7 +22,182 @@ def index():
     # return render_template('index.html', categories=cates, pages=page_size)
     return render_template('index.html', pages=page_size)
 
+<<<<<<< Updated upstream
 @app.route("/login")
+=======
+# @app.route("/send_email/<email>", methods=['get'])
+# def send_email(email):
+#     password = request.json.get('password')
+#     if current_user.is_authenticated:
+#         msg_title = "Cập nhật mật khẩu"
+#         sender = "noreply@app.com"
+#         msg = Message(msg_title, sender=sender, recipients=[email])
+#         msg_body = "Bạn đã đặt lại mật khẩu là"
+#         data = {
+#             'title': msg_title,
+#             'body': msg_body,
+#             'user_name': current_user.username,
+#             'password': password
+#         }
+#         msg.html = render_template("email.html", data=data)
+#
+#         try:
+#             mail.send(msg)
+#             return jsonify({
+#                 "title": "Email sent...."
+#             })
+#         except Exception as e:
+#             print(e)
+#             return jsonify({
+#                 "title": "The Email was not sent"
+#             })
+
+
+@app.route("/lookuppoints", methods=['get'])
+def lookuppoints():
+    ms = ""
+    aleart = ""
+    years = dao.load_year()
+    semesters = dao.load_semester(1)
+    year = request.args.get('year')
+    semester = request.args.get('semester')
+    subject_score = {}
+    subjects = []
+    score_types = []
+    average_subject = {}
+    if current_user.is_authenticated:
+        if year is None and semester is None:
+            ms = "Chưa chọn dữ liệu"
+            aleart = "warning"
+        elif (current_user.user_role.__eq__(UserRole.USER)):
+            class_grade_id = dao.get_classgrade_id(current_user.id)
+            subjects = dao.load_subject(semester_id=int(semester), class_grade_id=class_grade_id)
+            for sub in subjects:
+                print(sub.name)
+            score_types = dao.load_score_type()
+            if subjects and score_types:
+                ms = "Đã tải dữ liệu hoàn tất"
+                aleart = "success"
+                for subject in subjects:
+                    dao_result = dao.get_average_score(subject_id=subject.id, student_id=current_user.id)
+                    average_subject[subject.id] = round(dao_result, 1)
+                    if subject.id not in subject_score:
+                        # Nếu chưa có subject.id thì tạo một mục mới cho môn học này
+                        subject_score[subject.id] = {}
+                    for score in score_types:
+                        subject_score[subject.id][score.id] = []
+
+                        score_datas = dao.load_score(
+                            student_id=current_user.id,
+                            score_type_id=score.id,
+                            subject_id=subject.id
+                        )
+
+                        # score_values=[]
+
+                        # for score_data in score_datas:
+                        #     score_values.append(score_data.so_diem)
+
+                        # Kiểm tra nếu có dữ liệu trả về từ dao.load_score
+                        if score_datas:
+                            subject_score[subject.id][score.id] = score_datas
+                        else:
+                            print(f"No score data for Subject ID: {subject.id}, Score ID: {score.name}")
+                        # for s in subject_score[subject.id][score.id]:
+                        #     print(s.so_diem)
+                    # print(subject_score[subject.id])
+            else:
+                ms = "Không có dữ liệu"
+                aleart = "delete"
+
+    return render_template('lookinguppoint.html', aleart=aleart, message=ms, years=years, subjects=subjects,
+                           subject_score=subject_score, score_types=score_types, semesters=semesters,
+                           average_subject=average_subject)
+
+
+@app.route('/lookuppoints/year/<int:year_id>', methods=['get'])
+def year_process(year_id):
+    semesters = dao.load_semester(year_id)
+    return jsonify({
+        "semesters_id": [semester.id for semester in semesters],
+        "semesters_name": [f"{semester.name}" for semester in semesters]
+    })
+
+
+@app.route("/regulationtostudent", methods=['get'])
+def regulation():
+    regulations = dao.load_regulation()
+    return render_template('regulation_student.html', regulations=regulations)
+
+
+@app.route('/regulationtostudent/<int:regulation_id>', methods=['get'])
+def regulation_process(regulation_id):
+    regulation = dao.load_regulation(regulation_id)
+    regulation_detail = dao.load_detail_regulation(regulation_id)
+    return jsonify({
+        "name": regulation.name,
+        "id": regulation.id,
+        "max_students": getattr(regulation_detail, 'max_students', None),
+        "min_age": getattr(regulation_detail, 'min_age', None),
+        "max_age": getattr(regulation_detail, 'max_age', None),
+    })
+
+
+@app.route("/changeinfo", methods=['get', 'post'])
+def changeinfo():
+    msg = ""
+    alert = ""
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    address = request.form.get('address')
+    currentPassword = request.form.get('currentPassword')
+    newPassword = request.form.get('newPassword')
+    confirmPassword = request.form.get('confirmPassword')
+    avatar = request.files.get('avatar')
+    print(avatar)
+    if current_user.is_authenticated:
+        if current_user.user_role.__eq__(UserRole.USER):
+            if email or phone or address or avatar:
+                if dao.change_information(current_user.id, email, phone, address, avatar):
+                    msg = "Thông tin đã được cập nhật"
+                    alert = "success"
+            else:
+                msg = "Không có thông tin để cập nhật"
+                alert = "warning"
+            if currentPassword and confirmPassword and newPassword:
+                if dao.check_password(newPassword, confirmPassword):
+                    if dao.change_password(current_user.id, currentPassword, newPassword):
+                        msg = "Cập nhật mật khẩu thành công"
+                        alert = "success"
+                        # mesg_title = "Cập nhật mật khẩu"
+                        # sender = "noreply@app.com"
+                        # message = Message(mesg_title, sender=sender, recipients=[current_user.email])
+                        # mesg_body = "Bạn đã đặt lại mật khẩu là"
+                        # data = {
+                        #     'title': mesg_title,
+                        #     'body': mesg_body,
+                        #     'user_name': current_user.name,
+                        #     'password': newPassword
+                        # }
+                        # message.html = render_template("email.html", data=data)
+                        # try:
+                        #     mail.send(message)
+                        #     msg = "Cập nhật mật khẩu thành công"
+                        #     alert = "success"
+                        # except Exception as e:
+                        #     print(e)
+                        #     msg = "Lỗi không gửi được email"
+                        #     alert = "delete"
+                    else:
+                        msg = "Lỗi không cập nhật được mật khẩu"
+                        alert = "delete"
+                else:
+                    msg = "Mật khẩu không khớp"
+                    alert = "delete"
+    return render_template('changeinfo_student.html', msg=msg, alert=alert)
+
+@app.route("/login", methods=['get', 'post'])
+>>>>>>> Stashed changes
 def login_process():
     return render_template('login.html')
 
